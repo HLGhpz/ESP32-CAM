c0b527ed6b63b58f4ea63d654dcda29dbb26b0ee
hlghpz <1090377416@qq.com>
2019-10-03 23:22:49 +0800

    加入了自己输入iip

--- miniprogram/pages/cam/cam.js
+++ miniprogram/pages/cam/cam.js
@@ -1,4 +1,5 @@
 // miniprogram/pages/cam.js
+
 Page({
 
     /**
@@ -11,7 +12,7 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: function(options) {
         // wx.cloud.callFunction({
         //     name:'getimage',
         // }).then(res=>{
@@ -25,55 +26,63 @@ Page({
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {
+    onReady: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
+    onShow: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function () {
+    onHide: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function () {
+    onUnload: function() {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
+    onPullDownRefresh: function() {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {
+    onReachBottom: function() {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
+    onShareAppMessage: function() {
 
     },
 
-    get_image: function (event) {
+    get_image: function(event) {
+        console.log(event);
         this.setData({
-            url: 'http://192.168.43.220:81/stream'
+            url: `http://${this.data.ip}:81/stream`
+        })
+    },
+
+    onChange: function(event){
+        this.setData({
+            ip: event.detail
         })
     }
+
 })
